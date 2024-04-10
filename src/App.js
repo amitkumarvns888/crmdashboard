d3662f82764093ff1d15bc04255ad42d5d405f60
amit kumar <amit.aptron@gmail.com>
2024-04-10 10:08:01 +0530

    initial chamge

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
